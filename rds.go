628d31620743fe29c69622e0cb9754b3f1d38f27
TesterlifeRaymond <58558327@qq.com>
2019-10-29 10:47:38 +0800

    Create rds.go

---
+++ rds.go
@@ -0,0 +1,15 @@
+package rds
+
+import "github.com/go-redis/redis"
+
+func NewRedis(config *redis.Options) *redis.Client {
+	return redis.NewClient(config)
+}
+
+func NewRedisConf(host, password string, db int) *redis.Options {
+	return &redis.Options{
+		Addr:     host,
+		Password: password,
+		DB:       db,
+	}
+}
